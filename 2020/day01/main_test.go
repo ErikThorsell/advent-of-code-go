a6917178be0994ea2c20298ff2e9e78ac9250d43
Erik Thorsell <erik@thorsell.cc>
2020-12-01 06:22:24 +0100

    Add tests for Day01, 2020

--- 2020/day01/main_test.go
+++ 2020/day01/main_test.go
@@ -1,16 +1,32 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func TestMain(t *testing.T) {
+func TestPart1(t *testing.T) {
 
-	inputs := []int{1, 2}
-	expectedOutputs := []int{0, 0}
+	inputs := [][]int{{1721, 979, 366, 299, 675, 1456}}
+	expectedOutputs := []int{514579}
 
 	for i := 0; i < len(inputs); i++ {
-		actual := part1(inputs)
-		if expectedOutputs[i] != actual {
-			t.Errorf("Test failed, expected: '%d', got:  '%d'", expectedOutputs, actual)
+		actual := part1(inputs[i])
+		if !reflect.DeepEqual(expectedOutputs[i], actual) {
+			t.Errorf("Test failed, expected: '%d', got:  '%d'", expectedOutputs[i], actual)
+		}
+	}
+}
+
+func TestPart2(t *testing.T) {
+
+	inputs := [][]int{{1721, 979, 366, 299, 675, 1456}}
+	expectedOutputs := []int{241861950}
+
+	for i := 0; i < len(inputs); i++ {
+		actual := part2(inputs[i])
+		if !reflect.DeepEqual(expectedOutputs[i], actual) {
+			t.Errorf("Test failed, expected: '%d', got:  '%d'", expectedOutputs[i], actual)
 		}
 	}
 }
