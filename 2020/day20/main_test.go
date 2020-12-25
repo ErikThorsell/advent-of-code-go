199662cc4f03059152ebb02d980827059c35d80d
Erik Thorsell <erik@thorsell.cc>
2020-12-25 14:48:47 +0100

    Add tests for Day 20, 2020

--- 2020/day20/main_test.go
+++ 2020/day20/main_test.go
@@ -7,27 +7,13 @@ import (
 	"github.com/ErikThorsell/advent-of-code-go/util"
 )
 
-func Test0(t *testing.T) {
-
-	exampleData, _ := ioutil.ReadFile("./example0")
-	exampleResult := 0
-
-	parsedExampleData := util.ParseInputByBlankLine(string(exampleData))
-	actual := part1(parsedExampleData)
-
-	if exampleResult != actual {
-		t.Errorf("Test failed, expected: '%d', got:  '%d'", exampleResult, actual)
-	}
-}
-
 func Test1(t *testing.T) {
 
-	exampleData, _ := ioutil.ReadFile("../data/20")
-	//exampleData, _ := ioutil.ReadFile("./example")
-	exampleResult := 0
+	exampleData, _ := ioutil.ReadFile("./example")
+	exampleResult := 20899048083289
 
 	parsedExampleData := util.ParseInputByBlankLine(string(exampleData))
-	actual := part1(parsedExampleData)
+	actual, _, _ := part1(parsedExampleData)
 
 	if exampleResult != actual {
 		t.Errorf("Test failed, expected: '%d', got:  '%d'", exampleResult, actual)
@@ -37,10 +23,11 @@ func Test1(t *testing.T) {
 func Test2(t *testing.T) {
 
 	exampleData, _ := ioutil.ReadFile("./example")
-	exampleResult := 0
+	exampleResult := 273
 
-	parsedExampleData := util.ParseInputByLine(string(exampleData))
-	actual := part2(parsedExampleData)
+	parsedExampleData := util.ParseInputByBlankLine(string(exampleData))
+	_, tiles, idGrid := part1(parsedExampleData)
+	actual := part2(tiles, idGrid)
 
 	if exampleResult != actual {
 		t.Errorf("Test failed, expected: '%d', got:  '%d'", exampleResult, actual)
